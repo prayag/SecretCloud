0ad259cd0509e916bfb0c1171e32219f9c15362d
Prayag Narula <prayag@fairtradework.com>
2011-05-26 14:55:55 -0500

    Added real time edge detection. TODO: Use rec when working on windows
    Added RealTimeEdgeDetection using processing and added lines that uses
    rec and can be used in lieu of ffmpeg

--- speechToText.sh
+++ speechToText.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ffmpeg -f oss -i /dev/dsp -ar 16000 -ab 512000 ffmpeg.flac
 wget -q -U "Mozilla/5.0" --post-file ffmpeg.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium"
+
 rm -rf ffmpeg.flac
